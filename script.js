94b943874f6f53eebd01c893e537b90c7683a223
Tej Bari <136843098+tejbari5@users.noreply.github.com>
2024-01-09 19:56:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+
+if(window.innerWidth>window.innerHeight){
+	document.body.style.backgroundImage=`url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Paysage-en-orientation-paysage.jpg')`
+}else{
+	document.body.style.backgroundImage=`url('https://photovideocreative.com/wordpress/wp-content/uploads/2017/11/Portrait-en-orientation-portrait.jpg')`
+}
